6c0747ca907d932f5727652a362b7b7b80392bd9
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2018-12-11 22:02:54 +0100

    Update terminal.h

--- src/terminal.h
+++ src/terminal.h
@@ -4,7 +4,7 @@ int term_putchar(char c);
 /* Scrive un int su terminale */
 int term_putint(int x);
 /* Scrive un unsigned int su terminale */
-int term_putunsigned(unsigned int x);
+int term_putunsigned(unsigned int n);
 /* Scrive una stringa su terminale */
 int term_puts(char *str);
 /* Legge un char da terminale */

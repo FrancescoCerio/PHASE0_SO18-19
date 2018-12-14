75658af06c4f0c6f22c9fb8a8e4714477930c588
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2018-12-14 09:22:44 +0100

    Update terminal.h

--- src/terminal.h
+++ src/terminal.h
@@ -11,7 +11,7 @@ int term_puts(char *str);
 /* Legge un char da terminale */
 int term_getchar(void);
 /* Salva in buf una stringa letta da terminale */
-void readline(char *buf, unsigned int count)
+void readline(char *buf, unsigned int count);
 
 
 /* Gestione macchina uMPS */

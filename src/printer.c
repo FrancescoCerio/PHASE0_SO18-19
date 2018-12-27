9a457fc1453a2457a9236f269942128963f5392a
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2018-12-27 17:25:57 +0100

    Update printer.c

--- src/printer.c
+++ src/printer.c
@@ -3,13 +3,12 @@
 #include "printer.h"
 
 typedef unsigned int u32;
-
+/* Puntatore alla struttura dati utilizzata dalla stampante */
 static volatile dtpreg_t *printer = (dtpreg_t *) DEV_REG_ADDR(IL_PRINTER,0);
 
 int print_putchar(char c){
-	u32 stat=printer->status; 
-	
-	switch(stat){
+	/* Eseguo un controllo sullo stato della stampante prima dell'operazione di stampa */
+	switch(printer->status){
 		case 0:
 			term_puts("\nDEVICE NOT INSTALLED\n");
 			return -1;
@@ -27,14 +26,16 @@ int print_putchar(char c){
 			return -1;
 			break;
 	}
-
+	/* Il campo command indica il tipo di operazione che la stampante deve eseguire. */
+	/* Il numero 2 corrisponde all'operazioni di stampa del valore salvato in data0. */
 	printer->data0=c;
 	printer->command=2;
-
+	
 	return 0;
 }
 
 int print_str(char *str){
+	/* Per ogni carattere di str eseguo l'operazione di print_putchar e controllo se viene eseguita con successo */
 	for (; *str; ++str){
 		if (print_putchar(*str))
 			return -1;

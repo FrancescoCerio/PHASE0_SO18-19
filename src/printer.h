3d27cec6691630f6cef7b624be032e43a3ff3fe3
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2018-12-27 17:34:41 +0100

    Update printer.h

--- src/printer.h
+++ src/printer.h
@@ -1,5 +1,4 @@
-/* Funzioni che permettono di utilizzare la stampante di uMPS */
-
+/* Funzioni per la gestione della stampante di uMPS */
 void printer_reset();
 void printer_ack();
 int print_putchar(char c);

cb5d3ecf114ed6ae136e201c1dbad1154b2d32b7
francesco_cerio <francescocerio98@gmail.com>
2018-12-27 04:03:08 -0800

    Update disk.h

--- src/disk.h
+++ src/disk.h
@@ -1,3 +1,9 @@
-unsigned int disk_status();
+typedef unsigned int u32;
+
+int disk_write(u32 *ptr_current_ram);
+int disk_read(u32 *ptr_current_ram);
+u32 disk_status();
+void disk_chdata0(unsigned int chdata);
 void disk_reset();
-unsigned int disk_seel(unsigned int cylnum);
+void disk_ack();
+u32 disk_seek(u32 cylnum);

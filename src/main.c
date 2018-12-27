a441f2a4ea0b8f2e4e5abba6c7b5475ada309110
francesco_cerio <francescocerio98@gmail.com>
2018-12-27 04:03:35 -0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include "umps/arch.h"
 #include "terminal.h"
 #include "printer.h"
+#include "disk.h"
 
 #define LINE_BUF_SIZE 64
 #define TOD 0x1000001c			        /*  Numero di istruzioni svolte dalla CPU   */
@@ -14,20 +15,27 @@ static char buf[LINE_BUF_SIZE];
 int main(int argc, char *argv[]){
     term_puts("TEST PER MACCHINA UMPS\n");
     
-    /* Test I/O su terminale
+    /* Test I/O su terminale 
     term_puts("TEST LETTURA/SCRITTURA DA BUFFER\n");
     term_puts("INPUT");
     readline(buf,LINE_BUF_SIZE);
     term_puts("\nHo letto:");
     term_puts(buf);
+    
     term_puts("\nProva stampa valore int: 123->");
     term_putint(123);
-	*/
-	
+
     readline(buf,LINE_BUF_SIZE);
     print_str(buf);
 	
+    readline(buf, LINE_BUF_SIZE);
 	
-
+*/
+    disk_reset();
+	u32 *p = 0x20005000;
+	*p = 43;
+	disk_chdata0(0x20005000);
+	disk_write(*p);
+	disk_read(*p);
     return 0;
 }

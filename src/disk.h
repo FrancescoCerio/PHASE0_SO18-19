f1962edfe04dd3faae1e4b66c681b008b5d33314
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2018-12-27 16:32:06 +0100

    Update disk.h

--- src/disk.h
+++ src/disk.h
@@ -3,6 +3,7 @@ typedef unsigned int u32;
 int disk_write(u32 *ptr_current_ram, u32 head, u32 sect);
 int disk_read(u32 *ptr_current_ram, u32 head, u32 sect);
 u32 disk_status();
+void disk_info();
 void disk_chdata0(unsigned int chdata);
 void disk_reset();
 void disk_ack();

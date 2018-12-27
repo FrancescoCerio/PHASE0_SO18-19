fd4b9c3285722375617bfd19f615c682af3b21d2
francesco_cerio <francescocerio98@gmail.com>
2018-12-27 11:01:41 -0800

    Update disk.h

--- src/disk.h
+++ src/disk.h
@@ -1,9 +1,10 @@
+/*Dichiaro le funzioni necessarie per la gestione dei dischi */
 typedef unsigned int u32;
 
 int disk_write(u32 *ptr_current_ram, u32 head, u32 sect);
 int disk_read(u32 *ptr_current_ram, u32 head, u32 sect);
 u32 disk_status();
-void disk_chdata0(unsigned int chdata);
+void disk_info();
 void disk_reset();
 void disk_ack();
 u32 disk_seek(u32 cylnum);

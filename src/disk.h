ad1f8a52822ff25a0c8256d1b064f69e2dea7cd7
filippobartolucci <37805761+filippobartolucci@users.noreply.github.com>
2018-12-29 15:35:57 +0100

    Update disk.h

--- src/disk.h
+++ src/disk.h
@@ -1,8 +1,8 @@
 /*Dichiaro le funzioni necessarie per la gestione dei dischi */
 typedef unsigned int u32;
 
-int disk_write(u32 *ptr_current_ram, u32 head, u32 sect);
-int disk_read(u32 *ptr_current_ram, u32 head, u32 sect);
+u32 disk_write(u32 *ptr_current_ram, u32 head, u32 sect);
+u32 disk_read(u32 *ptr_current_ram, u32 head, u32 sect);
 u32 disk_status();
 void disk_info();
 void disk_reset();
